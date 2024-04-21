29cc7eb0532c74afcf79213b9c6dd82e78b90fd4
Jesús <js312597@fh-muenster.de>
2024-04-21 18:10:45 +0200

    quitamos eventos

--- script.js
+++ script.js
@@ -224,17 +224,6 @@ window.addEventListener('resize', onWindowResize, false);
 init3D();
 
 if (!!window.EventSource) {
-    var source = new EventSource('/events');
-  
-    source.addEventListener('open', function(e) {
-      console.log("Events Connected");
-    }, false);
-  
-    source.addEventListener('error', function(e) {
-      if (e.target.readyState != EventSource.OPEN) {
-        console.log("Events Disconnected");
-      }
-    }, false);
   
     source.addEventListener('gyro_readings', function(e) {
       //console.log("gyro_readings", e.data);
